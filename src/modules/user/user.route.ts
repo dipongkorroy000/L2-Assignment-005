20904fd262c40880d7101fd6033c3cc27dfd7ec5
Dipongkor Roy <dipongkorroy000@gmail.com>
2025-09-15 01:58:14 +0600

    super admin add & user role update by superadmin task completed

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -19,6 +19,8 @@ router.patch("/", authorize(Role.sender, Role.receiver), UserControllers.updateP
 router.get("/all-users", authorize(Role.admin, Role.super_admin), UserControllers.getAllUsers);
 
 // update user role by {email, role} = req.body;
+// super admin can make super admin & admin & sender & receiver
+// and admin can make admin & sender & receiver
 router.patch("/updateUserRole", authorize(Role.admin, Role.super_admin), UserControllers.updateUserRole);
 
 export const UserRoute = router;
